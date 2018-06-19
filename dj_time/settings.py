41454976928d9a8f2abcf3849d8db275abfb138b
Markus Münzer <kentravian@hotmail.com>
2018-06-19 21:05:20 +0200

    key

--- dj_time/settings.py
+++ dj_time/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_4yltx9=!=xsm)p6&32rbq1p7af-wpcfebja1hu@qqf%7l)$zi'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
